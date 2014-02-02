0338d3d2aa03e0fe2dc66e3494df6ed2956a62b6
CtrlFreak1337 <paultrepack1@gmail.com>
2014-02-02 15:15:58 -0500

    Potentiometer Code
    Added support for Potentiometer usage.

--- src/com/robot/main/BasicBot.java
+++ src/com/robot/main/BasicBot.java
@@ -1,28 +1,34 @@
 package com.robot.main;
 
-// import com.robot.in.DigitalIOSet;
-// import com.robot.out.Arm;
-import com.robot.out.MotorSet;
-
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Timer;
+//import com.robot.in.DigitalIOSet;
+//import com.robot.out.Arm;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.ADXL345_I2C;
 
 public class BasicBot extends IterativeRobot
 {
-	MotorSet drv = new MotorSet(1,2)/*,motors = new MotorSet(6,12)*/;
+	// MotorSet drv = new MotorSet(1,2)/*,motors = new MotorSet(6,12)*/;
 	// DigitalIOSet dio = new DigitalIOSet(1,12);
 	Joystick j1/*,j2*/;
-	// Jaguar leftDriveMotor,rightDriveMotor;
+	Jaguar ldm,rdm;
+	RobotDrive chassis;
+	ADXL345_I2C acl;
 	// double mag,theta,rotation;
 	// Arm arm;
-	
+
 	public void robotInit()
 	{
 		{ // Motor initialization
 			// arm = new Arm(5);
 		}
 		j1 = new Joystick(1);
+		ldm = new Jaguar(1);
+		rdm = new Jaguar(2);
+		chassis = new RobotDrive(rdm,ldm);
+		acl = new ADXL345_I2C(1,ADXL345_I2C.DataFormat_Range.k2G);
 		// j2 = new Joystick(2);
 
 		{ // DigitalIOSet initialization
@@ -39,15 +45,15 @@ public class BasicBot extends IterativeRobot
 		{
 			// Autonomous code goes here
 		}
-		drv.update();
+		// drv.update();
 		// motors.update();
 	}
 
 	public void teleopInit()
 	{
-		
+
 	}
-	
+
 	public void teleopPeriodic()
 	{
 		// dio.update();
@@ -71,52 +77,9 @@ public class BasicBot extends IterativeRobot
 		 * rotation = j1axes[2];
 		 * chassis.mecanumDrive_Polar(mag,theta,rotation);
 		 */
-		    
-	        double leftMotorSpeed,rightMotorSpeed,moveValue = j1.getY(),
-	        rotateValue = j1.getX();
-	        boolean squaredInputs = false;
-	        if(Math.abs(moveValue) > 1)
-	        	moveValue = moveValue/Math.abs(moveValue);
-	        if(Math.abs(rotateValue) > 1)
-	        	rotateValue = rotateValue/Math.abs(rotateValue);
-	        
 
-	        if (squaredInputs) {
-	            // square the inputs (while preserving the sign) to increase fine control while permitting full power
-	            if (moveValue >= 0.0) {
-	                moveValue = (moveValue * moveValue);
-	            } else {
-	                moveValue = -(moveValue * moveValue);
-	            }
-	            if (rotateValue >= 0.0) {
-	                rotateValue = (rotateValue * rotateValue);
-	            } else {
-	                rotateValue = -(rotateValue * rotateValue);
-	            }
-	        }
-
-	        if (moveValue > 0.0) {
-	            if (rotateValue > 0.0) {
-	                leftMotorSpeed = moveValue - rotateValue;
-	                rightMotorSpeed = Math.max(moveValue, rotateValue);
-	            } else {
-	                leftMotorSpeed = Math.max(moveValue, -rotateValue);
-	                rightMotorSpeed = moveValue + rotateValue;
-	            }
-	        } else {
-	            if (rotateValue > 0.0) {
-	                leftMotorSpeed = -Math.max(-moveValue, rotateValue);
-	                rightMotorSpeed = moveValue + rotateValue;
-	            } else {
-	                leftMotorSpeed = moveValue - rotateValue;
-	                rightMotorSpeed = -Math.max(-moveValue, -rotateValue);
-	            }
-	        }
-
-	        drv.setValue(1,leftMotorSpeed);
-	        drv.setValue(2,rightMotorSpeed);
-	        drv.update();
-	        Timer.delay(0.01d);
-	        
+		chassis.arcadeDrive(acl.getAcceleration(ADXL345_I2C.Axes.kY),acl.getAcceleration(ADXL345_I2C.Axes.kX),false);
+		System.out.println("Joystick X: " + j1.getX() + "\nJoystick Y: " + j1.getY());
+		System.out.println("Left Motor Value: " + ldm.get() + "\nRight Motor Value: " + rdm.get());
 	}
 }
